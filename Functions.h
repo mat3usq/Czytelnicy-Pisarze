b64675630b9b46e3d103a726153ce04eba020be2
mat3usq <121086125+mat3usq@users.noreply.github.com>
2023-06-06 14:15:42 +0200

    Update Functions.h

--- Functions.h
+++ Functions.h
@@ -1,40 +1,115 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
-#include <string.h>
+#include "Functions.h"
 
-unsigned int readersCount = 10;
-unsigned int writersCount = 10;
-unsigned int readingPersons = 0;
-unsigned int writingPersons = 0;
+pthread_mutex_t mutex;
+pthread_cond_t canWrite;
+pthread_cond_t canRead;
+unsigned int waitingReaders = 0;
+unsigned int waitingWriters = 0;
 
-void options(int N, char **argv)
+void startReading()
 {
-    for (int i = 0; i < N; i++)
-        if (strcmp(argv[i], "-R") == 0)
-            readersCount = atoi(argv[i + 1]);
-        else if (strcmp(argv[i], "-W") == 0)
-            writersCount = atoi(argv[i + 1]);
+    pthread_mutex_lock(&mutex);
+    if (waitingWriters > 0 || writingPersons > 0)
+    {
+        waitingReaders++;
+        pthread_cond_wait(&canRead, &mutex);
+    }
+    else
+    {
+        readingPersons++;
+        info();
+    }
+    pthread_mutex_unlock(&mutex);
 }
 
-void info()
+void stopReading()
 {
-    printf("ReaderQ: %i WriterQ: %i [in: R: %i W: %i]\n", readersCount - readingPersons, writersCount - writingPersons, readingPersons, writingPersons);
+    pthread_mutex_lock(&mutex);
+    readingPersons--;
+
+    if (readingPersons == 0)
+        pthread_cond_signal(&canWrite);
+
+    pthread_mutex_unlock(&mutex);
 }
 
-int randomNumber()
+void startWriting()
 {
-    srand(time(NULL)); // Inicjalizacja generatora liczb losowych
+    pthread_mutex_lock(&mutex);
 
-    int lowerBound = 100000;
-    int upperBound = 1000000; // 2^16
+    if (readingPersons > 0 || writingPersons > 0)
+    {
+        waitingWriters++;
+        pthread_cond_wait(&canWrite, &mutex);
+        waitingWriters--;
+    }
 
-    // Obliczanie zakresu losowych liczb
-    int range = upperBound - lowerBound + 1;
+    writingPersons = 1;
+    info();
+    pthread_mutex_unlock(&mutex);
+}
 
-    // Generowanie losowej liczby i dodawanie wartości dolnego zakresu
-    int randomNumber = rand() % range + lowerBound;
+void stopWriting()
+{
+    pthread_mutex_lock(&mutex);
+    writingPersons = 0;
 
-    return randomNumber;
+    if (waitingReaders == 0)
+        pthread_cond_signal(&canWrite);
+    else
+    {
+        pthread_cond_broadcast(&canRead);
+        readingPersons += waitingReaders;
+        waitingReaders = 0;
+        info();
+    }
+
+    pthread_mutex_unlock(&mutex);
+}
+
+void *reader(void *arg)
+{
+    while (1)
+    {
+        startReading();
+        usleep(randomNumber());
+        stopReading();
+        usleep(randomNumber());
+    }
+}
+
+void *writer(void *arg)
+{
+    while (1)
+    {
+        startWriting();
+        usleep(randomNumber());
+        stopWriting();
+        usleep(randomNumber());
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    if (options(argc, argv))
+    {
+        printf("./[PROGRAM] -R [LB CZYTELNIKOW] -W [LB PISARZY]\n");
+        return -1;
+    }
+
+    pthread_mutex_init(&mutex, NULL);
+    pthread_cond_init(&canWrite, NULL);
+    pthread_cond_init(&canRead, NULL);
+
+    pthread_t writers[writersCount];
+    pthread_t readers[readersCount];
+
+    for (int i = 0; i < writersCount; i++)
+        pthread_create(&writers[i], NULL, writer, NULL);
+    for (int i = 0; i < readersCount; i++)
+        pthread_create(&readers[i], NULL, reader, NULL);
+    for (int i = 0; i < writersCount; i++)
+        pthread_join(writers[i], NULL);
+    for (int i = 0; i < readersCount; i++)
+        pthread_join(readers[i], NULL);
 }
