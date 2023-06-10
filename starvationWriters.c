3549e764ff387abb3dfd31a7ba7cb345002e4693
mat3usq <121086125+mat3usq@users.noreply.github.com>
2023-06-10 22:54:42 +0200

    komentarze

--- starvationWriters.c
+++ starvationWriters.c
@@ -1,34 +1,40 @@
 #include "Functions.h"
 
-pthread_mutex_t readerMutex;
-pthread_mutex_t writerMutex;
+pthread_mutex_t readerMutex;    // Mutex dla czytelników
+pthread_mutex_t writerMutex;    // Mutex dla pisarzy
 
 void *reader(void *arg)
 {
-    srand(time(NULL));
+    srand(time(NULL));           // Inicjalizacja generatora liczb losowych
     while (1)
     {
-        // Poczatek Czytania
-        pthread_mutex_lock(&readerMutex);
+        // Początek Czytania
+        pthread_mutex_lock(&readerMutex);      // Zablokowanie mutexa dla czytelników
+
         if (readingPersons == 0)
-            pthread_mutex_lock(&writerMutex);
-        readingPersons++;
+            pthread_mutex_lock(&writerMutex);  // Jeśli nie ma żadnych czytelników, blokuj mutex dla pisarzy
+
+        readingPersons++;                       // Zwiększenie liczby czytelników
         info();
-        pthread_mutex_unlock(&readerMutex);
+
+        pthread_mutex_unlock(&readerMutex);    // Odblokowanie mutexa dla czytelników
 
         // Czas Czytania
         usleep(randomTime());
 
         // Koniec Czytania
-        pthread_mutex_lock(&readerMutex);
-        readingPersons--;
-        info();
-        if (readingPersons == 0)
-            pthread_mutex_unlock(&writerMutex);
-        pthread_mutex_unlock(&readerMutex);
+        pthread_mutex_lock(&readerMutex);       // Zablokowanie mutexa dla czytelników
 
-        // Wyjscie z czytelni
-        // Dojscie do kolejki
+        readingPersons--;                       // Zmniejszenie liczby czytelników
+        info();
+
+        if (readingPersons == 0)
+            pthread_mutex_unlock(&writerMutex); // Jeśli nie ma żadnych czytelników, odblokuj mutex dla pisarzy
+
+        pthread_mutex_unlock(&readerMutex);     // Odblokowanie mutexa dla czytelników
+
+        // Wyjście z czytelni
+        // Dojście do kolejki
         usleep(randomTime());
     }
     return NULL;
@@ -36,24 +42,26 @@ void *reader(void *arg)
 
 void *writer(void *arg)
 {
-    srand(time(NULL));
+    srand(time(NULL));           // Inicjalizacja generatora liczb losowych
     while (1)
     {
-        // Poczatek Pisania
-        pthread_mutex_lock(&writerMutex);
-        writingPersons++;
+        // Początek Pisania
+        pthread_mutex_lock(&writerMutex);      // Zablokowanie mutexa dla pisarzy
+
+        writingPersons++;                       // Zwiększenie liczby pisarzy
         info();
 
         // Czas pisania
         usleep(randomTime());
 
         // Koniec Pisania
-        writingPersons--;
+        writingPersons--;                       // Zmniejszenie liczby pisarzy
         info();
-        pthread_mutex_unlock(&writerMutex);
 
-        // Wyjscie z czytelni
-        // Dojscie do kolejki
+        pthread_mutex_unlock(&writerMutex);    // Odblokowanie mutexa dla pisarzy
+
+        // Wyjście z czytelni
+        // Dojście do kolejki
         usleep(randomTime());
     }
     return NULL;
@@ -61,30 +69,29 @@ void *writer(void *arg)
 
 int main(int argc, char **argv)
 {
-
     if (options(argc, argv))
     {
         printf("./[PROGRAM] -R [LB CZYTELNIKOW] -W [LB PISARZY]\n");
         return -1;
     }
 
-    pthread_mutex_init(&readerMutex, NULL);
-    pthread_mutex_init(&writerMutex, NULL);
+    pthread_mutex_init(&readerMutex, NULL);     // Inicjalizacja mutexa dla czytelników
+    pthread_mutex_init(&writerMutex, NULL);     // Inicjalizacja mutexa dla pisarzy
 
-    pthread_t writingPersons[writersCount];
-    pthread_t readingPersons[readersCount];
-
-    for (int i = 0; i < writersCount; i++)
-        pthread_create(&writingPersons[i], NULL, writer, NULL);
-
-    for (int i = 0; i < readersCount; i++)
-        pthread_create(&readingPersons[i], NULL, reader, NULL);
+    pthread_t writingPersons[writersCount];     // Tablica wątków pisarzy
+    pthread_t readingPersons[readersCount];     // Tablica wątków czytelników
 
     for (int i = 0; i < writersCount; i++)
-        pthread_join(writingPersons[i], NULL);
+        pthread_create(&writingPersons[i], NULL, writer, NULL);     // Tworzenie wątków pisarzy
 
     for (int i = 0; i < readersCount; i++)
-        pthread_join(readingPersons[i], NULL);
+        pthread_create(&readingPersons[i], NULL, reader, NULL);     // Tworzenie wątków czytelników
+
+    for (int i = 0; i < writersCount; i++)
+        pthread_join(writingPersons[i], NULL);                      // Oczekiwanie na zakończenie wątków pisarzy
+
+    for (int i = 0; i < readersCount; i++)
+        pthread_join(readingPersons[i], NULL);                      // Oczekiwanie na zakończenie wątków czytelników
 
     return 0;
 }
